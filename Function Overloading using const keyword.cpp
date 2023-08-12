fb433937791dc331e0ab578972baba347e342d89
Rishabh Yadav <1998rishabh31@gmail.com>
2023-08-12 16:57:53 +0530

    Create Function Overloading using const keyword.cpp

---
+++ Function Overloading using const keyword.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+class Test 
+{
+    private:
+	    int x;
+    public:
+	    Test(int i): x(i){}
+	
+	    void fun() const 
+	        { cout << "fun() const called " << endl;}
+	    void fun() 
+	        { cout << "fun() called " << endl; }
+};
+
+int main()
+{
+	Test t1(10);
+	const Test t2(20);
+	t1.fun();
+	t2.fun();
+	return 0;
+}
