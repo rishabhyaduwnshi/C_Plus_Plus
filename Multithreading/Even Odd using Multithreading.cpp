cac9428d4b4b72bfdc535777bb32e767b79141d5
Rishabh Yadav <1998rishabh31@gmail.com>
2023-09-03 13:43:15 +0530

    Create Even Odd using Multithreading.cpp

---
+++ Multithreading/Even Odd using Multithreading.cpp
@@ -0,0 +1,62 @@
+#include <iostream>
+#include <chrono>
+#include <thread>
+
+typedef unsigned long long ull;
+
+ull even_sum = 0;
+ull odd_sum = 0;
+
+void find_odd_sum(ull start, ull end)
+{
+    for (ull i = start; i < end; i++)
+    {
+        if (i % 2 == 1)
+            odd_sum += i;
+    }
+}
+
+
+void find_even_sum(ull start, ull end)
+{
+    for (ull i = start; i < end; i++)
+    {
+        if (i % 2 == 0)
+            even_sum += i;
+    }
+}
+
+int main()
+{
+    ull start = 0;
+    ull end = 1000000000;
+
+    //get the time before starting the calculation
+    auto start_time = std::chrono::high_resolution_clock::now();
+    
+
+    //initialise the threads
+    std::thread t1(find_odd_sum, start, end);
+    std::thread t2(find_even_sum, start, end);
+
+
+    //join threads
+    t1.join();
+    t2.join();
+
+
+    //get the time after completing the calculation
+    auto end_time = std::chrono::high_resolution_clock::now();
+
+    //find the duration
+    auto duration = std::chrono::duration_cast<std::chrono::duration<double>>(end_time - start_time);
+
+    //even & odd sum results
+    std::cout <<"Odd sum is : " << odd_sum <<std::endl;
+    std::cout <<"Even sum is : " << even_sum << std::endl;
+
+    //print the duration
+    std::cout <<"Time required for this compuation was : " << duration.count() << std::endl;
+
+}
+
