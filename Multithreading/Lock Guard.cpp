389869cce00c8760700f6ee95cb75013c71feb2c
Rishabh Yadav <1998rishabh31@gmail.com>
2023-09-09 12:23:43 +0530

    Create Lock Guard.cpp

---
+++ Multithreading/Lock Guard.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <thread>
+#include <mutex>
+
+std::mutex my_mutex;
+int buffer = 1;
+
+void incrementBuffer(int thread_id, int n)
+{
+	std::lock_guard <std::mutex> lock(my_mutex);
+	for (int i = 1; i <= n; i++)
+		std::cout << "Thread ID : " << thread_id << std::endl<< "Current Buffer Value : " << buffer++ << std::endl;
+}
+
+int main()
+{
+	std::thread t1(incrementBuffer, 1, 10);
+	std::thread t2(incrementBuffer, 2, 10);
+
+	t1.join();
+	t2.join();
+}
