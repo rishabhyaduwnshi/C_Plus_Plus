d3de3ac8cbfc91565779f8a4af9e144b3efd0561
Rishabh Yadav <1998rishabh31@gmail.com>
2023-09-09 11:27:30 +0530

    Create Try Lock Until.cpp

---
+++ Multithreading/Try Lock Until.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <thread>
+#include <chrono>
+#include <mutex>
+
+
+std::timed_mutex my_mutex;
+int my_money = 0;
+
+void increment(int thread_number)
+{
+	auto now = std::chrono::steady_clock::now();
+	if (my_mutex.try_lock_until(now + std::chrono::seconds(1)))
+	{
+		my_money++;
+		std::this_thread::sleep_for(std::chrono::seconds(2));
+		std::cout << "Thread number : " << thread_number << " Entered\n";
+		my_mutex.unlock();
+	}
+	else
+	{
+		std::cout << "Thread number : " << thread_number << " cound not enter\n";
+	}
+
+}
+
+int main()
+{
+	std::thread t1(increment, 1);
+	std::thread t2(increment, 2);
+
+	t1.join();
+	t2.join();
+
+	std::cout << "Total Money is : " << my_money;
+}
